acb1fa51cec363b426689609446cc4257be9c913
Leonardo Minora <leonardo.minora@gmail.com>
2023-09-26 11:53:17 -0300

    passo 02 - adicionado bibliotecas e configurações padrões

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      estado: 'ok',
+      mensagem: 'API Online',
+      dados: 'API exemplo da disciplina de POS',
+    };
   }
 }
