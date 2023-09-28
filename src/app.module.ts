0a671edcf1e766799798195814d6f5edee953987
Leonardo Minora <leonardo.minora@gmail.com>
2023-09-28 10:44:55 -0300

    passo 03 - aticionado rota /tarefas e codificado GET e POST

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { config } from './ormconfig';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), TarefasModule],
   controllers: [AppController],
   providers: [AppService],
 })
